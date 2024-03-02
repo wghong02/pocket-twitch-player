40c8ec6041105f020bfa1c9e72d7142f3dfef0c7
Wanchen <wghong@bu.edu>
2024-03-02 13:26:20 -0500

    first

---
+++ src/test/java/com/projects/twitch/TwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.twitch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

98284eba40e42e7efafa4fde2592baa9bfb8646c
Wanchen <wghong@bu.edu>
2024-03-07 21:38:15 -0500

    added more classes

---
+++ src/main/java/com/projects/twitch/item/ItemController.java
@@ -0,0 +1,24 @@
+package com.projects.twitch.item;
+
+import com.projects.twitch.model.TypeGroupedItemList;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ItemController {
+
+
+    private final ItemService itemService;
+
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+
+    @GetMapping("/search")
+    public TypeGroupedItemList search(@RequestParam("game_id") String gameId) {
+        return itemService.getItems(gameId);
+    }
+}
